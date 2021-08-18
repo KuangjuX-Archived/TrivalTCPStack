67ea2d6c93cd6c3afad989ccd56e7ab5cfe3e6a9
KuangjuX <qcx@tju.edu.cn>
2021-08-18 07:54:21 -0700

    thread pool

--- include/thread_pool.h
+++ include/thread_pool.h
@@ -10,25 +10,7 @@ typedef struct {
     handler* table[THREAD_POOL_SIZE];
 } thread_pool;
 
-thread_pool* pool;
+extern thread_pool* pool;
 
-int apply() {
-    pthread_mutex_lock(&pool->lock);
-    int index;
-    for (int i = 0; i < THREAD_POOL_SIZE; i++) {
-        if (!pool->table[THREAD_POOL_SIZE]->allocated) {
-            pool->table[i]->allocated = TRUE;
-            index = i;
-        }
-    }
-    pthread_mutex_unlock(&pool->lock);
-    return index;
-}
-
-void destory(int index) {
-    pthread_mutex_lock(&pool->lock);
-    pthread_cancel(*pool->table[index]->thread);
-    pool->table[index]->thread = NULL;
-    pool->table[index]->allocated = FALSE;
-    pthread_mutex_unlock(&pool->lock);
-}
+int apply();
+void destory(int index);
