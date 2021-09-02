61f64c60e4d54752e816c32a0a583fc96f868189
KuangjuX <qcx@tju.edu.cn>
2021-09-01 22:59:53 -0700

    update

--- include/trivial_epoll.h
+++ include/trivial_epoll.h
@@ -2,5 +2,16 @@
 #define _TRIVIAL_EPOLL_H_
 
 #define MAX_EVENT_NUMS 1024
+#define MAX_EPOLL_SIZE 1024
+
+typedef void (*udp_recv_callback)(int, struct sockaddr_in*, const char*, unsigned int);
+
+typedef struct epoll_server_t {
+    int epoll_fd;
+    int listen;
+    int stop_flag;
+
+    udp_recv_callback callback_handler;
+} epoll_server_t;
 
 #endif
--- src/trivial_epoll.c
+++ src/trivial_epoll.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/resource.h>
 #include "trivial_epoll.h"
 
 struct epoll_event* events;
@@ -36,6 +37,21 @@ int create_udp_listen(short port) {
     }
 
     return sockFd;
+}
+
+epoll_server_t* epoll_udp_server_init(unsigned port, udp_recv_callback handler, int max_client) {
+    epoll_server_t* eps = (epoll_server_t*)malloc(sizeof(epoll_server_t));
+    struct epoll_event ev;
+
+    eps->callback_handler = handler;
+    eps->listen = create_udp_listen(port);
+    eps->epoll_fd = epoll_create(MAX_EPOLL_SIZE);
+
+    ev.events = EPOLLIN;
+    ev.data.fd = eps->listen;
+    int ret = epoll_ctl(eps->epoll_fd, EPOLL_CTL_ADD, eps->listen, &ev);
+
+    return eps;
 }
 
 int epoll_event_loop() {
