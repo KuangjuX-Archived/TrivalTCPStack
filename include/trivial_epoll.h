2d8d387f92e4ae1ddd655f0fae5b3ce028c185bf
KuangjuX <qcx@tju.edu.cn>
2021-09-03 23:29:33 -0700

    add epoll struct

--- include/trivial_epoll.h
+++ include/trivial_epoll.h
@@ -1,8 +1,18 @@
 #include <pthread.h>
 
-typedef struct list_head {
 
+
+// Double list in linux
+typedef struct list_head {
+    struct list_head* prev;
+    struct list_head* next;
+    void* data;
 } list_head;
+
+typedef struct wait_queue_head_t {
+    pthread_mutex_t lock;
+    list_head task_list;
+} wait_queue_head_t;
 
 typedef struct rbroot {
 
@@ -14,6 +24,9 @@ typedef struct epitem {
 
 typedef struct eventepoll {
     pthread_mutex_t lock;
+
+    wait_queue_head_t wq;
+    wait_queue_head_t poll_wait;
 
     list_head rdllist;
 } eventepoll;
